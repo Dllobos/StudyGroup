1344ccc4e90bd02c3a50845899e4ce55afe548e6
Dllobos <dllobosr@gmail.com>
2016-06-02 13:11:36 -0400

    StudyGroup import optimizados
    Optimizar los import

--- StudyGroup/app/src/main/java/com/studygroup/studygroup/RegisterActivity.java
+++ StudyGroup/app/src/main/java/com/studygroup/studygroup/RegisterActivity.java
@@ -4,16 +4,13 @@ package com.studygroup.studygroup;
  * Created by Daniel Lobos
  */
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
